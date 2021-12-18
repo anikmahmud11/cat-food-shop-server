30916acbcb6ef70678a84db26809db0a291f1a5e
Anik <baust.cse.160201078@gmail.com>
2021-12-18 12:01:18 +0600

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const res = require('express/lib/response');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,25 +22,83 @@ async function run() {
         const database = client.db('cat-shop');
         const productCollection = database.collection('products')
         const orderCollection = database.collection('orders')
+        const reviewCollection = database.collection('review')
+        const userCollection = database.collection('users')
 
-       //GET Products API
-       app.get('/products', async (req, res) => {
-        const cursor = productCollection.find({});
-        const products = await cursor.toArray();
-        res.send(products);
+        //GET Products API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
 
         //Add order
         app.post('/orders', async (req, res) => {
-
             const order = req.body;
             console.log('order', order);
             const result = await orderCollection.insertOne(order);
             res.json(result);
 
         });
+        //Delete order Api
+        app.delete('/orders/:id', async (req, res) => {
+            var ObjectId = require('mongodb').ObjectID;
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            console.log('deleted', result);
+
+            res.json(result);
+        })
+
+        //GET order API
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+        //Add review
+        app.post('/review', async (req, res) => {
+
+            const review = req.body;
+            console.log('review', review);
+            const result = await reviewCollection.insertOne(review);
+            res.json(result)
+        });
+        //GET review API
+        app.get('/review', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+        //add user API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            res.json(result)
+        });
+        // //upsert
+        // app.put('/users', async (req, res) => {
+        //     const user = req.body;
+        //     console.log('put',user);
+        //     const filter = { email: user.email };
+        //     const option = { upsert: true };
+        //     const updateDoc = { $set: user };
+        //     const result = await userCollection.updateOne(filter,updateDoc,option);
+        //     res.json(result);
+        // })
+        //add Products API
+        app.post('/products', async (req, res) => {
+
+            const product = req.body;
+            console.log('product', product);
+            const result = await productCollection.insertOne(product);
+            res.json(result)
+         });
 
 
-    });
+
 
         console.log('connected to database');
     }
